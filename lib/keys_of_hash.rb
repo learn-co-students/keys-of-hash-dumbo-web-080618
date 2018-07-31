484afea5cfa9e829eb513dc26bec81062f8e4cff
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-31 19:16:38 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,25 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  # This is the single arg version
+  # def keys_of(arguments)
+  #   # code goes here
+  #     select{|k,v|
+  #       v == arguments
+  #     }.keys
+  # end
+  
+  # This works but could be tighter (less verbose, more succinct)
+  #   def keys_of(*arguments)
+  #     arr = []
+  #     arguments.each_with_index{|arg, i|
+  #       arr[i] = select{|k, v|
+  #         v == arg
+  #       }.keys
+  #     }
+  #     arr.flatten
+  # end
+  
+  # After consulting with the 
+  def keys_of(*arguments)
+    select{|k, v| arguments.include?(v)}.keys
   end
 end
